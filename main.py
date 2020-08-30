57caabaaae55ad5247b808e3e3d496e17a1ac4e8
LeeJiHey <gx530@naver.com>
2020-08-31 01:28:16 +0900

    python file made

---
+++ main.py
@@ -0,0 +1,15 @@
+## 순서
+
+# 0. home
+# 1. face_capture
+# 2. menu -> [select by face, select by thema, personal color]
+# 3-1. select by face
+#  -> eyebrow - eyeshadow - eyeliner - blusher - lip
+# 3-2. select by thema
+#  -> 미정
+# 3-3. personal color
+#  -> 미정
+# 4. frame (본격적인 화장 시작)
+#  1) base 메이크업 - 동영상 제공 (순서가 여기인지 확실x)
+#  2) 눈썹프레임 - 아이새됴우프레임 - 아이라이너 프레임 - 블러셔 프레임 - 립(프레임은 없음)
+# 5. 가상화장한 얼굴 - 실제 화장한 얼굴 비교
